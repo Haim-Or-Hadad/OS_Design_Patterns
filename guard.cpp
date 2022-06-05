8e2ff4a0957f340bd245f6b804dbdfbe4a31b724
Haim Or Hadad <93033782+Haim-Or@users.noreply.github.com>
2022-06-05 22:13:17 +0300

    Update guard.cpp

--- guard.cpp
+++ guard.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <thread>
+
+pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
 
 //exercise 5
 class guard
@@ -24,10 +27,35 @@ guard::~guard()
 pthread_mutex_unlock(this->lock);
 }
 
+int i=0;
+int c;
+void *print_message_function( void *ptr )
+{
+    guard g(&lock2);
+    std::cout << "thread "<< i << " start" << std::endl;
+    for (int i = 0; i < 25; i++)
+    {
+        c++;
+        (char*) ptr; // update ptr
+    } 
+    std::cout << "thread "<< i << " done"<< std::endl;
+    i++; 
+}
+
+
 
 int main()
 {
-    std::cout << "ilan test";
+     pthread_t thread1, thread2;
+     std::string m1 = "Thread 1";
+     std::string m2 = "Thread 2";
+     pthread_create( &thread1, NULL, print_message_function, &m1);
+     pthread_create( &thread2, NULL, print_message_function, &m2);
+     pthread_join( thread1, NULL);
+     pthread_join( thread2, NULL); 
+     std::cout << "Thread 1 returns: " << thread1 << std::endl;
+     std::cout << "Thread 2 returns: " << thread2 << std::endl;
+     exit(0);
 }
 
 /*
