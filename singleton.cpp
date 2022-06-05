5865c77d42eedd3835e0d4fcc0e2ff2fc7eee508
Haim-Or <haimor1123@gmail.com>
2022-06-05 18:34:31 +0300

    final version

--- singleton.cpp
+++ singleton.cpp
@@ -1,9 +1,13 @@
 #include <pthread.h>
+#include <string.h>
+#include <iostream>
 #include <mutex>
+#include <fstream>
+
 
 using namespace std;
 
-pthread_mutex_t single_locker;
+pthread_mutex_t single_locker = PTHREAD_MUTEX_INITIALIZER;
 static int flag = 0;
 
 template<typename T>
@@ -13,24 +17,28 @@ private:
 T object_;
 static singleton *singleton_obj;
 //constructor
-singelton(T object1){
-pthread_mutex_lock(&single_locker);
+singleton(T object1){
+//pthread_mutex_lock(&single_locker);
 this->object_ = object1;
-flag = 1;
+//flag = 1;
 }
+
 public:
 static singleton *Instance(T object1){
-if (flag == 1)
+pthread_mutex_lock(&single_locker);
+if (flag == 0)
 {
-    pthread_mutex_lock(&single_locker);
-    return singleton_obj;
+   singleton * singleton_obj = new singleton(object1);
+   cout << "create new object" << endl;
+   flag = 1;
 }
-flag = 1;
-singleton * singleton_obj = new singleton(object1);
+pthread_mutex_unlock(&single_locker);
 return singleton_obj;
 }
 
 void Destroy(){
+pthread_mutex_lock(&single_locker);
+delete singleton_obj;
 flag = 0;
 pthread_mutex_unlock(&single_locker);
 }
@@ -47,15 +55,24 @@ class pet {
 
 int main()
 {
+    ///////////////////////test-1//////////////////////////////
     pet *my_pet; 
     singleton<pet *> *instance_1 = singleton<pet *>::Instance(my_pet);
     singleton<pet *> *instance_2 = singleton<pet *>::Instance(my_pet);
     if (instance_1 != instance_2) {
-        printf("test case failed\n");
+        cout << "test case failed" << endl;;
+    }
+    cout << "test-1 passed" << endl;
+    instance_1->Destroy();
+    //////////////////////////test-2///////////////////////////
+    FILE *pointer_to_file = fopen("example.txt","R");
+    singleton<FILE *> *file_1 = singleton<FILE *>::Instance(pointer_to_file);
+    singleton<FILE *> *file_2 = singleton<FILE *>::Instance(pointer_to_file);
+    if (file_1 != file_2) {
+        cout << "test case failed" << endl;;
         exit(EXIT_FAILURE);
     }
-    printf("test passed\n");
+    cout << "test-2 passed" << endl;
+    file_1->Destroy();
     exit(EXIT_SUCCESS);
-
-    
 }
